5ada0ed3551c359f3314155ad44478854087ca73
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2018-10-16 21:36:21 +0200

    Add tests for ZMON client
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- go.mod
+++ go.mod
@@ -61,7 +61,7 @@ require (
 	github.com/soheilhy/cmux v0.1.4 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.2 // indirect
-	github.com/stretchr/testify v1.2.2 // indirect
+	github.com/stretchr/testify v1.2.2
 	github.com/tmc/grpc-websocket-proxy v0.0.0-20171017195756-830351dc03c6 // indirect
 	github.com/ugorji/go v1.1.1 // indirect
 	github.com/xiang90/probing v0.0.0-20160813154853-07dd2e8dfe18 // indirect
---
+++ pkg/zmon/zmon_test.go
@@ -0,0 +1,233 @@
+package zmon
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEntities(tt *testing.T) {
+	client := &http.Client{}
+	for _, ti := range []struct {
+		msg      string
+		filters  map[string]string
+		status   int
+		body     string
+		err      error
+		entities []Entity
+	}{
+		{
+			msg:      "test getting back a single entity",
+			filters:  map[string]string{"alias": "stups"},
+			status:   http.StatusOK,
+			body:     `[{"id": "x"}]`,
+			entities: []Entity{{ID: "x"}},
+		},
+		{
+			msg:     "test getting back a 500",
+			filters: map[string]string{"alias": "stups"},
+			status:  http.StatusInternalServerError,
+			body:    `{"error": 500}`,
+			err:     fmt.Errorf("unexpected response code: 500"),
+		},
+	} {
+		tt.Run(ti.msg, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(ti.status)
+					w.Write([]byte(ti.body))
+				}),
+			)
+			defer ts.Close()
+
+			zmonClient := NewZMONClient(ts.URL, "", client)
+			entities, err := zmonClient.Entities(ti.filters)
+			assert.Equal(t, ti.err, err)
+			assert.Len(t, entities, len(ti.entities))
+			assert.Equal(t, ti.entities, entities)
+		})
+	}
+
+}
+
+func TestQuery(tt *testing.T) {
+	client := &http.Client{}
+	for _, ti := range []struct {
+		msg         string
+		duration    time.Duration
+		aggregators []string
+		status      int
+		body        string
+		err         error
+		dataPoints  []DataPoint
+		key         string
+	}{
+		{
+			msg:      "test getting back a single data point",
+			duration: 1 * time.Hour,
+			status:   http.StatusOK,
+			body: `{
+			         "queries": [
+				   {
+				     "results": [
+				       {
+					 "values": [
+					  [1539710395000,765952]
+					]
+				       }
+				     ]
+				   }
+				 ]
+			}`,
+			dataPoints: []DataPoint{
+				{
+					Time:  time.Unix(1539710395, 0),
+					Value: 765952,
+				},
+			},
+		},
+		{
+			msg:      "test getting back a single datapoint with key",
+			duration: 1 * time.Hour,
+			status:   http.StatusOK,
+			key:      "my-key",
+			body: `{
+			         "queries": [
+				   {
+				     "results": [
+				       {
+					 "values": [
+					  [1539710395000,765952]
+					]
+				       }
+				     ]
+				   }
+				 ]
+			}`,
+			dataPoints: []DataPoint{
+				{
+					Time:  time.Unix(1539710395, 0),
+					Value: 765952,
+				},
+			},
+		},
+		{
+			msg:         "test getting back a single datapoint with aggregators",
+			duration:    1 * time.Hour,
+			status:      http.StatusOK,
+			aggregators: []string{"max"},
+			body: `{
+			         "queries": [
+				   {
+				     "results": [
+				       {
+					 "values": [
+					  [1539710395000,765952]
+					]
+				       }
+				     ]
+				   }
+				 ]
+			}`,
+			dataPoints: []DataPoint{
+				{
+					Time:  time.Unix(1539710395, 0),
+					Value: 765952,
+				},
+			},
+		},
+		{
+			msg:         "test query with invalid aggregator",
+			aggregators: []string{"invalid"},
+			err:         fmt.Errorf("invalid aggregator 'invalid'"),
+		},
+		{
+			msg:    "test query with invalid response",
+			status: http.StatusInternalServerError,
+			body:   `{"error": 500}`,
+			err:    fmt.Errorf("[kariosdb query] unexpected response code: 500"),
+		},
+		{
+			msg:      "test getting invalid values response",
+			duration: 1 * time.Hour,
+			status:   http.StatusOK,
+			body: `{
+			         "queries": [
+				   {
+				     "results": [
+				       {
+					 "values": [
+					  [1539710395000,765952,1]
+					]
+				       }
+				     ]
+				   }
+				 ]
+			}`,
+			err: fmt.Errorf("[kariosdb query] unexpected response data"),
+		},
+	} {
+		tt.Run(ti.msg, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(ti.status)
+					w.Write([]byte(ti.body))
+				}),
+			)
+			defer ts.Close()
+
+			zmonClient := NewZMONClient("", ts.URL, client)
+			dataPoints, err := zmonClient.Query(1, ti.key, nil, ti.aggregators, ti.duration)
+			assert.Equal(t, ti.err, err)
+			assert.Len(t, dataPoints, len(ti.dataPoints))
+			assert.Equal(t, ti.dataPoints, dataPoints)
+		})
+	}
+
+}
+
+func TestKariosDBEntityFormat(t *testing.T) {
+	entity := "aws:1234567:eu-central-1:kube-1[pod-1]"
+	assert.Equal(t, "aws_1234567_eu-central-1_kube-1_pod-1_", KairosDBEntityFormat(entity))
+}
+
+func TestDurationToSampling(tt *testing.T) {
+	for _, ti := range []struct {
+		msg      string
+		duration time.Duration
+		sampling sampling
+	}{
+		{
+			msg:      "1 hour should map to hours sampling",
+			duration: 1 * time.Hour,
+			sampling: sampling{
+				Unit:  "hours",
+				Value: 1,
+			},
+		},
+		{
+			msg:      "2 years should map to years sampling",
+			duration: 2 * day * 365,
+			sampling: sampling{
+				Unit:  "years",
+				Value: 2,
+			},
+		},
+		{
+			msg:      "1 nanosecond should map to 0 milliseconds sampling",
+			duration: 1,
+			sampling: sampling{
+				Unit:  "milliseconds",
+				Value: 0,
+			},
+		},
+	} {
+		tt.Run(ti.msg, func(t *testing.T) {
+			assert.Equal(t, durationToSampling(ti.duration), ti.sampling)
+		})
+	}
+}
