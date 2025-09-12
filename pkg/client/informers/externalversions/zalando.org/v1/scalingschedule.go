a485ee7942949404aa42945b02fef95b814448cb
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2025-09-12 09:30:53 +0200

    Regenerate code
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- pkg/client/informers/externalversions/zalando.org/v1/scalingschedule.go
+++ pkg/client/informers/externalversions/zalando.org/v1/scalingschedule.go
@@ -62,13 +62,25 @@ func NewFilteredScalingScheduleInformer(client versioned.Interface, namespace st
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ZalandoV1().ScalingSchedules(namespace).List(context.TODO(), options)
+				return client.ZalandoV1().ScalingSchedules(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ZalandoV1().ScalingSchedules(namespace).Watch(context.TODO(), options)
+				return client.ZalandoV1().ScalingSchedules(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ZalandoV1().ScalingSchedules(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ZalandoV1().ScalingSchedules(namespace).Watch(ctx, options)
 			},
 		},
 		&apiszalandoorgv1.ScalingSchedule{},
