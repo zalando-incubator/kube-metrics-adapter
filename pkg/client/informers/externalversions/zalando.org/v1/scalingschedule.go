25bd53a98737116eef20872a5b40ee510adb5611
Martin Linkhorst <m.linkhorst+github.com@gmail.com>
2026-03-23 12:05:52 +0100

    chore(deps): update k8s.io/* to v0.35.3
    Signed-off-by: Martin Linkhorst <m.linkhorst+github.com@gmail.com>

--- pkg/client/informers/externalversions/zalando.org/v1/scalingschedule.go
+++ pkg/client/informers/externalversions/zalando.org/v1/scalingschedule.go
@@ -57,7 +57,7 @@ func NewScalingScheduleInformer(client versioned.Interface, namespace string, re
 // one. This reduces memory footprint and number of connections to the server.
 func NewFilteredScalingScheduleInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
-		&cache.ListWatch{
+		cache.ToListWatcherWithWatchListSemantics(&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
@@ -82,7 +82,7 @@ func NewFilteredScalingScheduleInformer(client versioned.Interface, namespace st
 				}
 				return client.ZalandoV1().ScalingSchedules(namespace).Watch(ctx, options)
 			},
-		},
+		}, client),
 		&apiszalandoorgv1.ScalingSchedule{},
 		resyncPeriod,
 		indexers,
